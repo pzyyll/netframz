5a7d7380bbebdba3f951831fdf884dff02ea6c63
caizhili <pzyyll@gmail.com>
2017-07-07 20:50:45 +0800

    test timer

--- main.cpp
+++ main.cpp
@@ -2,35 +2,25 @@
 #include <unistd.h>
 #include "src/nf_event.h"
 
-void cbfunc(EventLoop &loop, IOTask &task, int mask) {
-  std::cout << task.get_fd() << std::endl;
-  std::cout << mask << std::endl;
-  task.Stop(loop);
+void cbfunc(EventLoop &loop, Timer &task, int mask) {
+  std::cout << "Timer: " <<  mask << std::endl;
 }
 
 class Cb {
  public:
-     void CbFunc(EventLoop &loop, IOTask &task, int mask) {
-         std::cout << task.get_fd() << std::endl;
-         std::cout << mask << std::endl;
-         std::cout << "i=" << i << std::endl;
-         task.Stop(loop);
-     }
+  void cbfunc(EventLoop &loop, Timer &task, int mask) {
+    std::cout << "Timer: " <<  mask << i << std::endl;
+  }
 
-     int i;
+  int i;
 };
 
 int main() {
   EventLoop loop;
-  IOTask task(STDIN_FILENO, EV_POLLIN);
-  task.Bind(cbfunc);
-  task.Start(loop);
+  Timer timer1(1000, 0);
+  timer1.Bind(cbfunc);
+  timer1.Start(loop);
 
-  Cb cb;
-  cb.i = 33;
-  IOTask taskout(STDOUT_FILENO, EV_POLLOUT);
-  taskout.Bind(&Cb::CbFunc, cb);
-  taskout.Start(loop);
   loop.Run();
   return 0;
 }
