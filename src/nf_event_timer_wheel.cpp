1d679761c5faf4bdbb119e3ccf3b62649328fa24
caizhili-linode <pzyyll@gmail.com>
2017-07-11 14:48:08 +0800

    timer wheel

--- src/nf_event_timer_wheel.cpp
+++ src/nf_event_timer_wheel.cpp
@@ -3,3 +3,50 @@
 // @bref Timer implement by timing wheel
 //
 
+#include "nf_event_timer_wheel.h"
+
+#define NFTVN_BITS (6)
+#define NFTVR_BITS (8)
+#define NFTVN_SIZE (1 << NFTVN_BITS)
+#define NFTVR_SIZE (1 << NFTVR_BITS)
+#define NFTVN_MASK (NFTVN_SIZE - 1)
+#define NFTVR_MASK (NFTVR_SIZE - 1)
+#define LV_SHIFT(x) (NFTVR_BITS + (x)*NFTVN_BITS)
+
+void TimerWheel::_InnerAddTimer(const tnode_type &tnode) {
+  timer_list_t *vec = NULL;
+
+  if ((vec = _GetTimerListVec(tnode.expires)) != NULL)
+    vec->push_back(tnode);
+}
+
+timer_list_t* _GetTimerListVec(const unsigned long expires) {
+  unsigned long delta = expires - base_.clk;
+  timer_list_t *vec = NULL;
+
+  if (delta < (1 << LV_SHIFT(0))) {
+    unsigned idx = expires & NFTVR_MASK;
+    vec = base_.tv1.vec + idx;
+  } else if (delta < (1 << LV_SHIFT(1))) {
+    unsigned idx = (delta >> LV_SHIFT(0)) & NFTVN_MASK;
+    vec = base_.tv2.vec + idx;
+  } else if (delta < (1 << LV_SHIFT(2))) {
+    unsigned idx = (delta >> LV_SHIFT(1)) & NFTVN_MASK;
+    vec = base_.tv3.vec + idx;
+  } else if (delta < (1 << LV_SHIFT(3))) {
+    unsigned idx = (delta >> LV_SHIFT(2)) & NFTVN_MASK;
+    vec = base_.tv4.vec + idx;
+  } else if ((long)delta < 0) {
+    vec = base_.tv1.vec + (base_.clk) & NFTVR_MASK;
+  } else {
+    if (delta > 0xffffffffUL) {
+      delta = 0xffffffffUL;
+      expires = delta + base_.clk;
+    }
+
+    unsigned idx = (expires >> LV_SHIFT(3)) & NFTVN_MASK;
+    vec = base_.tv5.vec + idx;
+  }
+
+  return vec;
+}
--- src/nf_event_timer_wheel.h
+++ src/nf_event_timer_wheel.h
@@ -14,17 +14,12 @@
 #include <sys/time.h>
 #include "nf_event_timer.h"
 
-#define NFTVN_BITS (6)
-#define NFTVR_BITS (8)
-#define NFTVN_SIZE (1 << NFTVN_BITS)
-#define NFTVR_SIZE (1 << NFTVR_BITS)
-#define NFTVN_MASK (NFTVN_SIZE - 1)
-#define NFTVR_MASK (NFTVR_SIZE - 1)
 #define PRECISION_USEC (1e5)
 
 static inline unsigned long GetJiffersFromTime(const struct timeval &t)
 {
-    return (t.tv_sec * (unsigned long)(1e6 / PRECISION_USEC) + t.tv_usec / (unsigned long)PRECISION_USEC);
+  return (t.tv_sec * (unsigned long)(1e6 / PRECISION_USEC)
+          + t.tv_usec / (unsigned long)PRECISION_USEC);
 }
 
 static inline unsigned long GetDeafaultJiffers() {
@@ -37,8 +32,6 @@ struct TimerNode {
   unsigned long expires;
   Timer timer;
 };
-
-//todo first
 
 typedef std::list<TimerNode> timer_list_t;
 
@@ -53,26 +46,34 @@ typedef struct NFTvec {
 class TimerWheel {
  public:
   typedef Timer timer_type;
+  typedef std::vector<timer_type> timer_vec_type;
+  typedef TimerNode tnode_type;
 
  public:
 
   void Init();
 
-  int AddTimer(struct timeval expire, const timer_type &timer_data);
+  int AddTimer(const struct timeval &expire, const timer_type &timer_data);
 
   //放到主循环去获取，粒度依据循环的精度，最小是 PRECISION_USEC
-  int GetTimer(std::vector<timer_type> &all_timer);
+  int GetTimer(timer_vec_type &all_timer);
+
+ private:
+  void _InnerAddTimer(const tnode_type &tnode);
+  timer_list_t* _GetTimerListVec(const unsigned long expires);
+
+  int _TimerRun(timer_vec_type &all_timer, unsigned long jiffers);
 
  private:
   struct TvBase {
     //unsigned long size_;
     //节拍数，这里一节拍定为 100 ms;
-    unsigned long timer_jiffers_;
-    nftv_root_t tv1_;
-    nftv_t tv2_;
-    nftv_t tv3_;
-    nftv_t tv4_;
-    nftv_t tv5_;
+    unsigned long clk;
+    nftv_root_t tv1;
+    nftv_t tv2;
+    nftv_t tv3;
+    nftv_t tv4;
+    nftv_t tv5;
   } base_;
 };
 
