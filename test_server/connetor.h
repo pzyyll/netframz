2d26a598ad2dc43c73b077acafbff2aed23adb21
vm-centos7 <pzyyll@gmail.com>
2017-07-17 07:37:06 -0700

    nothing

--- test_server/connetor.h
+++ test_server/connetor.h
@@ -8,17 +8,34 @@
 #include <sys/time.h>
 #include "../src/nf_event_iotask.h"
 
+struct Buffer {
+    explicit Buffer(unsigned long size)
+        : m_base(new char[size]), m_size(size), m_npos(m_base) {
+
+    }
+
+    ~Buffer() {
+        if (m_base)
+            delete[] m_base;
+        m_base = NULL;
+    }
+
+    char *m_base;
+    unsigned long m_size;
+    char *m_npos;
+}
+
 class Connetor {
 public:
     Connetor();
-    ~Connetor();
+    virtual ~Connetor();
 
     int OnTimeOut(const unsigned long time_limit, const struct timeval &now);
 
 private:
     //second
     unsigned long last_act_time_;
-    IOTask *task;
+    IOTask *task_;
 };
 
 #endif //NF_TEST_CONNETOR_H
